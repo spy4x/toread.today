6ae0c78ac65eb8ed9a096759e9c64f3e389600a1
Anton Shubin <2spy4x@gmail.com>
2020-02-04 18:03:04 +0300

    Automatically close dropdown after selection

--- apps/client/src/app/protected/components/shared/dropdown/dropdown.directive.ts
+++ apps/client/src/app/protected/components/shared/dropdown/dropdown.directive.ts
@@ -7,11 +7,16 @@ import { LoggerService } from '../../../../services';
 export class DropdownDirective implements AfterViewInit, OnDestroy {
   dropdownEl: HTMLElement;
   triggerEl: HTMLElement;
+  menuEl: HTMLElement;
+  triggerElSelector = '.dropdown-trigger';
+  menuElSelector = '.dropdown-menu';
   isActiveClass = 'is-active';
   isHoverableClass = 'is-hoverable';
+  keepOnClickClass = 'keep-on-click';
   clickEventType = 'click';
   isActive = false;
   isMobileSafari = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+
   constructor(private dropdownElRef: ElementRef, private logger: LoggerService) { }
 
   ngAfterViewInit(): void {
@@ -26,7 +31,7 @@ export class DropdownDirective implements AfterViewInit, OnDestroy {
       this.dropdownEl.classList.remove(this.isHoverableClass);
     }
 
-    this.triggerEl = this.dropdownEl.querySelector('.dropdown-trigger') as HTMLElement;
+    this.triggerEl = this.dropdownEl.querySelector(this.triggerElSelector) as HTMLElement;
 
     if (!this.triggerEl) {
       return;
@@ -38,6 +43,8 @@ export class DropdownDirective implements AfterViewInit, OnDestroy {
       return;
     }
     window.addEventListener(this.clickEventType, this.onWindowClick.bind(this));
+
+    this.menuEl = this.dropdownEl.querySelector(this.menuElSelector) as HTMLElement;
   }
 
   ngOnDestroy(): void {
@@ -54,20 +61,23 @@ export class DropdownDirective implements AfterViewInit, OnDestroy {
 
   onTriggerClick(event: MouseEvent): void {
     event.preventDefault();
-    this.triggerIsActive();
+    this.toggle();
   }
 
   onWindowClick(event: MouseEvent): void {
-    if (!this.isActive) {
-      return;
+    if (this.isActive) {
+      const isDropdownClicked = this.isChildOf(event.target as HTMLElement, this.dropdownEl);
+      if (!isDropdownClicked) {
+        this.toggle();
+      }
     }
-    const isClosestToTrigger = this.isClosest(event.target as HTMLElement, this.dropdownEl);
-    if (!isClosestToTrigger) {
-      this.triggerIsActive();
+    const isMenuClicked = this.isChildOf(event.target as HTMLElement, this.menuEl);
+    if (isMenuClicked && !this.dropdownEl.classList.contains(this.keepOnClickClass)) {
+      this.hide();
     }
   }
 
-  triggerIsActive(): void {
+  toggle(): void {
     this.dropdownEl.classList.toggle(this.isActiveClass);
     this.isActive = this.dropdownEl.classList.contains(this.isActiveClass);
   }
@@ -78,17 +88,25 @@ export class DropdownDirective implements AfterViewInit, OnDestroy {
     }
   }
 
-  isClosest(element: HTMLElement, target: HTMLElement): boolean {
-    if (element === target) {
+  hide(): void {
+    this.dropdownEl.classList.remove(this.isActiveClass);
+    this.isActive = false;
+    const prev = this.dropdownEl.style.display;
+    this.dropdownEl.style.display = 'none';
+    setTimeout(() => this.dropdownEl.style.display = prev);
+  }
+
+  private isChildOf(child: HTMLElement, parent: HTMLElement): boolean {
+    if (child === parent) {
       return true;
     }
-    const actualParent = element.parentElement;
-    if (actualParent === target) {
+    const actualParent = child.parentElement;
+    if (actualParent === parent) {
       return true;
     }
     if (!actualParent) {
       return false;
     }
-    return this.isClosest(actualParent, target); // recursive search
+    return this.isChildOf(actualParent, parent); // recursive search
   }
 }
